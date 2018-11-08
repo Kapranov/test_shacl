4f068156417c7b155c75265f46123b6631290d58
Oleg G.Kapranov <lugatex@yahoo.com>
2018-11-08 19:27:41 +0200

    initial project

---
+++ lib/test_shacl.ex
@@ -0,0 +1,18 @@
+defmodule TestSHACL do
+  @moduledoc """
+  Documentation for TestSHACL.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestSHACL.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
